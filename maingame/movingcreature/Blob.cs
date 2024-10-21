5b84817b839b157ca529181f2d46bae41e24e682
Xeldlie <ct276@stud.uni-heidelberg.de>
2024-10-21 10:32:09 +0200

    blob changes

--- maingame/movingcreature/Blob.cs
+++ maingame/movingcreature/Blob.cs
@@ -5,25 +5,26 @@ public partial class Blob : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
-	public override void _PhysicsProcess(double delta)
+	
+	// name delta deltaT because it's the time between the frames
+	public override void _PhysicsProcess(double deltaT)
 	{
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			velocity += GetGravity() * (float)deltaT;
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		else if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
+		// Move left and right
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
