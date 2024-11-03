bf40f4645c0be861c91d1864545bafcd92a19056
calvalre <olga.sergeyeva@outlook.de>
2024-11-03 21:00:39 +0100

    animated a blob

--- maingame/Players/Blob.cs
+++ maingame/Players/Blob.cs
@@ -3,42 +3,42 @@ using System;
 
 public partial class Blob : CharacterBody2D
 {
-    public const float Speed = 300.0f;
-    public const float JumpVelocity = -400.0f;
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
 	
-    // name delta deltaT because it's the time between the frames
-    public override void _PhysicsProcess(double deltaT)
-    {
-        
-        // handle movement of blob
-        
-        Vector2 velocity = Velocity;
+	// name delta deltaT because it's the time between the frames
+	public override void _PhysicsProcess(double deltaT)
+	{
+		
+		// handle movement of blob
+		
+		Vector2 velocity = Velocity;
 
-        // add gravity
-        if (!IsOnFloor())
-        {
-            velocity += GetGravity() * (float)deltaT;
-        }
+		// add gravity
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)deltaT;
+		}
 
-        // handle Jump
-        else if (Input.IsActionJustPressed("blob_jump") && IsOnFloor())
-        {
-            velocity.Y = JumpVelocity;
-        }
+		// handle Jump
+		else if (Input.IsActionJustPressed("blob_jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
 
-        // get input direction using inputs and handle the movement/deceleration
-        // move left and right
-        Vector2 direction = Input.GetVector("blob_left", "blob_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
-        {
-            velocity.X = direction.X * Speed;
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-        }
+		// get input direction using inputs and handle the movement/deceleration
+		// move left and right
+		Vector2 direction = Input.GetVector("blob_left", "blob_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
